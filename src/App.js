67919d365dfa34cbbf036c8567a808929689d009
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-11-15 10:49:01 -0800

    got modal carousel working

--- src/App.js
+++ src/App.js
@@ -1,47 +1,149 @@
 import { useState } from "react";
 import photoCollection from "./data";
-import 'bootstrap'
+import "bootstrap";
 
 function App() {
+  const [filter, setFilter] = useState("all");
 
-  const [filter, setFilter] = useState('all')
+  const filteredPhotos =
+    filter === "all"
+      ? photoCollection
+      : photoCollection.filter((photo) => photo.category.includes(filter));
 
+  const [activeIndex, setActiveIndex] = useState(0);
 
-const filteredPhotos = filter === "all" ? photoCollection : photoCollection.filter(photo => photo.category.includes(filter))
-
-
+  const categories = [
+    "all",
+    "portrait",
+    "landscape",
+    "cinematic",
+    "abstract",
+    "product",
+    "medical",
+    "4x5",
+    "b&w",
+    "color",
+    "film",
+    "digital",
+  ];
 
   return (
     <div>
       <nav>
         <h1>DAN FINLEY PHOTO</h1>
-        <ul>
-          {["all", "landscape", "portrait", "abstract", "4x5", "b&w", "color", "film", "digital", "cinematic"].map(category => (
+
+        {/* FILTER menu */}
+        <ul id="filterlist">
+          {categories.map((category) => (
             <li key={category}>
-              <button onClick={() => setFilter(category)} style={{ color: filter === category ? 'gray' : 'orange' }}>
-                #{category}
-              </button>
+              <button onClick={() => setFilter(category)}>#{category}</button>
             </li>
           ))}
-          <li><a href='about.html'>#about</a></li>
-        </ul>        
+          <li>
+            <button>
+              <a href="about.html">#about</a>
+            </button>
+          </li>
+        </ul>
       </nav>
 
       <hr />
-    
-      <main>
-        <h2>{filter}</h2>
-          {filteredPhotos.map((image) => (
-            <img key={image.id} src={image.url} className="thumbnail" alt={image.description} />
-          ))}
-      </main>
-      
-      <footer>
-      {/* Footer */}
-      </footer>
-    </div>
-  )
-};
 
+      <main>
+        {/* Images Mapped */}
+        <h2>{filter}</h2>
+        <div class="row">
+          <div class="column">
+            {filteredPhotos.map((image) => (
+              <div
+                class="card-body"
+                id="altCard"
+                data-bs-toggle="modal"
+                data-bs-target="#altModal"
+              >
+                <img
+                  key={image.id}
+                  src={image.url}
+                  className="thumbnail"
+                  alt={image.description}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Modal */}
+        <div class="row">
+          <div class="col">
+            <div
+              class="modal fade"
+              id="altModal"
+              tabindex="-1"
+              role="dialog"
+              aria-hidden="true"
+            >
+              {/* // id points to carousel's data-bs-target */}
+
+              <div
+                className="modal-dialog modal-dialog-centered"
+                role="document"
+              >
+                <div className="modal-content">
+                  <div className="modal-body">
+                    {/* Carousel wrapper */}
+                    <div
+                      id="altCarousel"
+                      className="carousel slide"
+                      data-bs-ride="carousel"
+                    >
+                      {/* Image Slides */}
+                      <div className="carousel-inner">
+                        {filteredPhotos.map((image, index) => (
+                          <div
+                            key={index}
+                            className={`carousel-item ${
+                              index === activeIndex ? "active" : ""
+                            }`}
+                          >
+                            <img
+                              className="d-block w-100"
+                              src={image.url}
+                              alt={image.title}
+                            />
+                          </div>
+                        ))}
+                      </div>
+
+                      {/* Controls */}
+                      <button
+                        className="carousel-control-prev"
+                        type="button"
+                        data-bs-target="#altCarousel"
+                        data-bs-slide="prev"
+                      >
+                        <span className="carousel-control-prev-icon" />
+                      </button>
+
+                      <button
+                        className="carousel-control-next"
+                        type="button"
+                        data-bs-target="#altCarousel"
+                        data-bs-slide="next"
+                      >
+                        <span className="carousel-control-next-icon" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer>{/* Footer */}</footer>
+    </div>
+  );
+}
 
 export default App;
